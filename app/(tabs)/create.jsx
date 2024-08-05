b0ae0ab9237204ae0c7cc728f1098fffbe3ae80b
sai-myne <snm.tgi@gmail.com>
2024-08-05 10:31:09 +0800

    File picker

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -6,7 +6,7 @@ import FormField from '../../components/FormField'
 import CustomButton from '../../components/CustomButton'
 import { ResizeMode, Video } from 'expo-av'
 import { icons } from '../../constants'
-import * as DocumentPicker from 'expo-document-picker'
+import * as ImagePicker from 'expo-image-picker'
 import { router } from 'expo-router'
 import { createVideo } from '../../lib/appwrite'
 import { useGlobalContext } from '../../context/GlobalProvider'
@@ -22,8 +22,10 @@ const Create = () => {
   })
 
   const openPicker = async (selectType) => {
-    const result = await DocumentPicker.getDocumentAsync({
-      type: selectType === 'image' ? ['image/png', 'image/jpeg', 'image/jpg'] : ['video/mp4', 'video/gif']
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: selectType === 'image' ? ImagePicker.MediaTypeOptions.Images : ImagePicker.MediaTypeOptions.Videos,
+      aspect: [4, 3],
+      quality: 1
     })
     if(!result.canceled){
       if(selectType === 'image'){
